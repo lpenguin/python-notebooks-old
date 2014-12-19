0f120ab199f39354f01324192cf7a3da40bff6fe
n.pryanichnikov <n.pryanichnikov@vectordigital.ru>
2014-12-19 14:05:03 +0300

    orf streams implementation

---
+++ scala/rosalind/src/rosalind/assignments/orf.scala
@@ -0,0 +1,60 @@
+package rosalind.assignments
+
+import rosalind.util.FastaReader
+
+/**
+ * Created by nikita on 18.12.14.
+ */
+
+import rosalind.util.CodonTable
+
+object orf {
+  def main(args: Array[String]) {
+
+
+    def findGenes(s:Stream[Char]):List[String] = s match {
+      case 'M'#::xs =>
+        val (gene, rest) = cropGene(s)
+        if(rest.isEmpty){
+          Nil
+        }else{
+          gene.mkString :: findGenes(xs)
+        }
+      case x#::xs => findGenes(xs)
+      case Stream() => Nil
+
+    }
+
+    def cropGene(s:Stream[Char]) = {
+      s span (_ != '_')
+    }
+
+    def translateGene(s:Stream[Char]) = {
+      s sliding (3, 3) filter (_.size == 3) map (triplet => CodonTable.dna(triplet.mkString)) toStream
+    }
+
+    val dnaString = FastaReader.fromData("rosalind_orf_sample").head.value
+
+    val s = dnaString.toStream
+    val rs = s.reverse.map{
+      case 'T' => 'A'
+      case 'A' => 'T'
+      case 'G' => 'C'
+      case 'C' => 'G'
+    }
+
+    val ss = (0 to 2) flatMap (n => List(s drop n, rs drop n))
+
+    val set = new scala.collection.mutable.LinkedHashSet[String]
+
+    for(seq <- ss){
+      findGenes(translateGene(seq)) foreach {q =>
+        if(set.add(q)){
+          println(q)
+        }
+      }
+    }
+
+//    set foreach println
+  }
+}
--- scala/rosalind/src/rosalind/util/CodonTable.scala
+++ scala/rosalind/src/rosalind/util/CodonTable.scala
@@ -1,7 +1,8 @@
 package rosalind.util
 
+
 object CodonTable {
-  private val codonTableStr:String = """UUU F      CUU L      AUU I      GUU V
+  private val rnaCodonTableStr:String = """UUU F      CUU L      AUU I      GUU V
   UUC F      CUC L      AUC I      GUC V
   UUA L      CUA L      AUA I      GUA V
   UUG L      CUG L      AUG M      GUG V
@@ -17,26 +18,43 @@ object CodonTable {
   UGC C      CGC R      AGC S      GGC G
   UGA Stop   CGA R      AGA R      GGA G
   UGG W      CGG R      AGG R      GGG G"""
-  
-  val stopChar = '_'
-  private val aminoAcidsCounsMut = new scala.collection.mutable.HashMap[Char, Int]()
 
-  val table = codonTableStr.split("\\s+")
-  .sliding(2, 2)
-  .map((t:Array[String]) => {
-    val Array(nb, aa) = t
-    nb -> (aa match {
-      case "Stop" => stopChar
-      case x => x.head
-    })
-  }).toMap
-  
-  for((nb, aa) <- table){
-    val counts = aminoAcidsCounsMut.getOrElse(aa, 0)
-    aminoAcidsCounsMut(aa) = counts + 1
+  private val dnaCodonTableStr:String = """TTT F      CTT L      ATT I      GTT V
+TTC F      CTC L      ATC I      GTC V
+TTA L      CTA L      ATA I      GTA V
+TTG L      CTG L      ATG M      GTG V
+TCT S      CCT P      ACT T      GCT A
+TCC S      CCC P      ACC T      GCC A
+TCA S      CCA P      ACA T      GCA A
+TCG S      CCG P      ACG T      GCG A
+TAT Y      CAT H      AAT N      GAT D
+TAC Y      CAC H      AAC N      GAC D
+TAA Stop   CAA Q      AAA K      GAA E
+TAG Stop   CAG Q      AAG K      GAG E
+TGT C      CGT R      AGT S      GGT G
+TGC C      CGC R      AGC S      GGC G
+TGA Stop   CGA R      AGA R      GGA G
+TGG W      CGG R      AGG R      GGG G"""
+
+
+  def codonStringToTable(s:String) = {
+    val aminoAcidsCountsMut = new scala.collection.mutable.HashMap[Char, Int]()
+
+    s.split("\\s+")
+      .sliding(2, 2)
+      .map((t:Array[String]) => {
+      val Array(nb, aa) = t
+      nb -> (aa match {
+        case "Stop" => stopChar
+        case x => x.head
+      })
+    }).toMap
   }
-  
-  val aminoAcidsCounts = aminoAcidsCounsMut.toMap
-  
- 
+
+  val stopChar = '_'
+  val startAminoAcid = 'M'
+  val rna = codonStringToTable(rnaCodonTableStr)
+  val dna = codonStringToTable(dnaCodonTableStr)
+
+
 }
