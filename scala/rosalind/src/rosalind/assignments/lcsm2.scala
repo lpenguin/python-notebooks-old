07d43b6b103f3e598bff856b5306e7fe98465982
n.pryanichnikov <n.pryanichnikov@vectordigital.ru>
2014-12-29 17:47:59 +0300

    Added python solution

--- scala/rosalind/src/rosalind/assignments/lcsm2.scala
+++ scala/rosalind/src/rosalind/assignments/lcsm2.scala
@@ -13,12 +13,41 @@ import scala.collection.mutable.ListBuffer
  * Created by nikita on 28.12.14.
  * http://rosalind.info/problems/lcsm/
  */
+
+/*
+Simple python solution:
+def substr_in_all(arr, part):
+  for dna in arr:
+    if dna.find(part)==-1:
+      return False
+  return True
+
+def common_substr(arr, l):
+  first = arr[0]
+  for i in range(len(first)-l+1):
+    part = first[i:i+l]
+    if substr_in_all(arr, part):
+      return part
+  return ""
+
+def longest_common_substr(arr):
+  l = 0; r = len(arr[0])
+
+  while l+1<r:
+    mid = (l+r) / 2
+    if common_substr(arr, mid)!="":
+      l=mid
+    else:
+      r=mid
+
+  return common_substr(arr, l)
+ */
 object lcsm2 {
   def main(args: Array[String]) {
     val data = FastaReader.fromData("rosalind_lcsm")
 
     val tree = new Tree(TreeNode("/".toStream)) with SuffixTree
-    tree.longestCommonString(data.map(_.value)).foreach( x => println(x))
+    tree longestCommonString data.map(_.value) foreach ( x => println(x))
     traceMem()
 
 //    TreePrinter.print(tree)
---
+++ scala/rosalind/src/rosalind/assignments/splc.scala
@@ -0,0 +1,11 @@
+package rosalind.assignments
+
+/**
+ * Created by nikita on 29.12.14.
+ * http://rosalind.info/problems/splc/
+ */
+object splc {
+  def main(args: Array[String]) {
+
+  }
+}
